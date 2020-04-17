aae2ccad0d3f20d077b11113f18d8372ba0d0e48
Sara R Mills <SaraRMills@gmail.com>
2020-04-17 11:01:47 -0400

    search/filter function mostly working w bugs

--- src/components/Bestiary/Bestiary.js
+++ src/components/Bestiary/Bestiary.js
@@ -6,7 +6,9 @@ import Monsters from './Monsters'
 
 class Bestiary extends Component {
   state = {
-    monsters: []
+    monsters: [],
+    sort: '',
+    param: ''
   }
 
   componentDidMount() {
@@ -17,8 +19,21 @@ class Bestiary extends Component {
       .catch()
   }
 
-  renderMonsters() {
-    const allMonsters = this.state.monsters
+  renderMonsters = () => {
+    let { sort, param, term } = this.state    
+    let allMonsters = this.state.monsters
+    console.log(allMonsters)
+    // if(!sort && !param) {
+    //   allMonsters = this.state.monsters
+    // }
+
+    // if(sort) {
+    //   allMonsters = this.renderSorted(sort)
+    // }
+
+    if(term) {
+      allMonsters = this.renderSearched(param, term)
+    }
 
     return allMonsters.map(mon =>
       <Monsters
@@ -38,15 +53,77 @@ class Bestiary extends Component {
         enemy_skill={mon.enemy_skill}
         user_name={mon.user_name}
       />
-    )
+  )
   }
-  
+
+  renderSearched = (param, term) => {
+    let allMonsters = this.state.monsters
+    let mons = []
+
+    if(!term || !param) {
+      return allMonsters
+    }
+
+    if(term) {
+      param = param.toLowerCase()
+      console.log(param)
+
+      for(let i=0; i < allMonsters.length; i++) {
+        for (let [key, value] of Object.entries(allMonsters[i])) {
+          if(key === param && value.includes(term)) {
+            mons.push(allMonsters[i])
+            return mons
+          }
+        }
+      }
+    }
+  }
+
+
+  renderSorted = (sort) => {
+    let allMonsters = this.state.monsters
+    let sortby = sort.toLowerCase()
+    // if (sortby === 'Name' || sortby === 'Location') {
+    //   allMonsters.sort((a, b) => {
+    //       return a.sortby.toLowerCase() > b.sortby.toLowerCase() ? 1 : 
+    //         a.sortby.toLowerCase() < b.sortby.toLowerCase() ? -1 : 0
+    //     })
+    // }
+
+    return allMonsters
+    // if (sort === 'Gil' || sort === 'EXP') {
+    //   allMonsters
+    //     .sort((a, b) => {
+    //       return a['Rating'] < b['Rating'] ? 1 : 
+    //         a['Rating'] > b['Rating'] ? -1 : 0
+    //     })
+    //   return allMonsters.sort()
+    // }
+  }
+
+  handleSort = ev =>  {
+    let sort = ev.target.value
+    this.setState({
+      sort: sort
+    })
+  }
+
+  handleSearch = ev => {
+    ev.preventDefault()
+    let term = ev.target.search.value
+    let param = ev.target['filter-by'].value
+    this.setState({
+      param: param,
+      term: term
+    })
+  }
+
   render() {
     // this.renderMonsters()
     return (
       <div>
         <Nav />
-        <OptionsForm />
+        <OptionsForm handleSort={this.handleSort} handleSearch={this.handleSearch} />
         <h1>Bestiary</h1>
           {this.renderMonsters()}
       </div>
--- src/components/OptionsForm/OptionsForm.js
+++ src/components/OptionsForm/OptionsForm.js
@@ -3,26 +3,36 @@ import { Link } from 'react-router-dom'
 
 class OptionsForm extends Component {
 
+  // handleOption = ev => {
+  //   let sort = ev.target.value
+  //   console.log(sort)
+
+  // }
+
   render () {
     return (
       <section id="bestiary-options">
       <label htmlFor="sort-by">Sort by:</label>
-      <select>
+      <select defaultValue="Name" onChange={this.props.handleSort}>
         <option>Name</option>
-        <option>HP</option>
         <option>GIL</option>
-        <option>Type</option>
-        <option>Level</option>
+        <option>Location</option>
         <option>EXP</option>
       </select>
-      <label htmlFor="filter-by">Filter by:</label>
-      <select>
+
+      <form name="search-form" id="search-form" onSubmit={this.props.handleSearch}>
+      <label htmlFor="filter-by">Search by:</label>
+      <select name="filter-by" id="filter-by">
         <option>Location</option>
-        {/* <option>Type</option>
-        <option>Disc</option> */}
+        <option>Name</option>
+        <option>Drops</option>
+        <option>Steal</option>
+        <option>Enemy Skill</option>
       </select>
-      <label htmlFor="search-bestiary">Search:</label>
-        <input type="text" name="search-bestiary" id="search-bestiary" />
+      <label htmlFor="search">Search:</label>
+        <input type="text" name="search" id="search" />
+        <button type='submit'>Search</button>
+        </form>
       <button>
         <Link to="/monster-form">
           Add New
