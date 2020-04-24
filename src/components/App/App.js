8ee1a101045be06851183bfd35f16b68b7aa70f4
Sara R Mills <SaraRMills@gmail.com>
2020-04-23 20:55:57 -0400

    styling, testing, cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import Landing from '../Landing/Landing'
 import Bestiary from '../Bestiary/Bestiary'
 import AddMonsterForm from '../AddMonsterForm/AddMonsterForm'
@@ -18,21 +18,21 @@ class App extends Component {
       <main className='App'>
 
         {this.state.displayError && <p className="error">Something went wrong</p>}
-        <Switch>
-          <Route exact path ={'/'} 
-          component={Landing} />
-          <Route path={'/bestiary'}
-          component={Bestiary} />
-          <Route path={'/monster-form'}
-          component={AddMonsterForm} />
-          <Route path={'/guide-form'}
-          component={SaveGuideForm} />
-          <Route path={'/PHS'}
-          component={PHS} />
-          <Route
-              component={NotFoundPage}
-            />
-        </Switch>
+          <Switch>
+            <Route exact path ={'/'} 
+            component={Landing} />
+            <Route path={'/bestiary'}
+            component={Bestiary} />
+            <Route path={'/monster-form'}
+            component={AddMonsterForm} />
+            <Route path={'/guide-form'}
+            component={SaveGuideForm} />
+            <Route path={'/PHS'}
+            component={PHS} />
+            <Route
+                component={NotFoundPage}
+              />
+          </Switch>
       </main>
     );
   }
